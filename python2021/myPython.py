effdfab6b82da37f7d6ce8481c8404009246f336
omy <mi_la_n@yahoo.co.jp>
2021-07-30 02:46:01 +0900

    20210730 : 差分比較はなんとかなった気がする。要デバッグだけど。

--- python2021/myPython.py
+++ python2021/myPython.py
@@ -56,26 +56,30 @@ class myData:
 
         elif self.type == '[Comment]\n':
             # コメントは同じ内容のコメントが多いので、値の全部をキーにして取り扱う。もはやキーだけでも成り立つのでは。
-            tempKey = self.getDictKey(','.join(map(str, data.values())), self.comments)
+            #tempKey = self.getDictKey(','.join(map(str, data.values())), self.comments)
+            tempKey = self.getDictKey(''.join(map(str, data)), self.comments)
             self.comments[tempKey] = data
 
         elif self.type == '[Shape]\n':
             # [Comment]と同様。値を全部キーにしてしまおう
-            tempKey = self.getDictKey(','.join(map(str, data.values())), self.shapes)
+            #tempKey = self.getDictKey(','.join(map(str, data.values())), self.shapes)
+            tempKey = self.getDictKey(''.join(map(str, data)), self.shapes)
             self.shapes[tempKey] = data
 
         elif self.type == '[Line]\n':
             # [Comment]と同様。値を全部キーにしてしまおう
-            tempKey = self.getDictKey(','.join(map(str, data.values())), self.lines)
+            #tempKey = self.getDictKey(','.join(map(str, data.values())), self.lines)
+            tempKey = self.getDictKey(''.join(map(str, data)), self.lines)
             self.lines[tempKey] = data
 
         else:
             # ここはもうなにが来るかわからんので、素直にType名と識別子でキーを作って、他を全部入れる。
-            tempKey = self.getDictKey(self.type, self.others)
+            #tempKey = self.getDictKey(self.type, self.others)
+            tempKey = self.getDictKey(''.join(map(str, data)), self.others)
             self.others[tempKey] = data
 
     def getDictKey(self, tempKey, data):
-        # もしキーが既存の場合、適当に後ろに識別子でも付ける。同じオブジェクトが存在している状況など意味不明だが、ツールとしてできてしまうので仕方ない。
+        # もしキーが既存の場合、適当に後ろに識別子でも付ける。同じオブジェクトが存在している状況は一部情報で意味不明だが（Relationなど）、ツールとしてできてしまうので仕方ない。
         # 識別子は適当。そもそも異常な状況なので最後に一覧として出すし。識別子が被らなければ何でもよい。日時や配列数でも。
         # ちなみにA5M2のバージョン2.14 以降は項目[ZOrder]が新設されているので、それっぽいキー項目+ZOrderで必ず一意になるため、そのときにはこの関数は死蔵してよい。
         if tempKey in data:
@@ -97,24 +101,41 @@ class myFactory:
         elif self.type == '[Entity]\n':
             # [Entity]部。いわゆるテーブル情報。
             return self.craeteEntity(data)
-        elif self.type in ('[Relation]\n', '[Comment]\n', '[Shape]\n', '[Line]\n'):
-            # いまのところわかっているやつ全部。[Relation]と[Comment]と[Shape]、[Line]。
-            return self.createDict(data)
-        # elif self.type in ["[Line]\n"]:
-        #     # 特に処理なし
-        #     return data
+        elif self.type == '[Relation]\n':
+            # [Relation]部。リレーション
+            return self.createRelation(data)
+        elif self.type in ('[Comment]\n', '[Shape]\n', '[Line]\n'):
+            # いまのところわかっているやつ全部。[Comment]と[Shape]、[Line]。
+            return self.createList(data)
         else:
             return data
 
-    def createDict(self, data):
-        # いまのところ[Relation]と[Comment]と[Shape]部。
-
-        tempDict = {}
+    def createList(self, data):
+        tempList = []
 
         for l in data:
-            tempDict[l[:l.find('=') + 1]] = l[l.find('=') + 1:]
+            tempList.append(l)
 
-        return tempDict
+        return tempList
+
+    def createRelation(self, data):
+        # [Relation]部。リレーション
+        tempRelations = {'Entity1=': None, 'Entity2=': None, 'Data': None}
+
+        tempData = []
+
+        for i, l in enumerate(data):
+
+            if l.startswith('Entity1='):
+                tempRelations['Entity1='] = l[l.find('=') + 1:]
+            elif l.startswith('Entity2='):
+                tempRelations['Entity2='] = l[l.find('=') + 1:]
+            else:
+                tempData.append(l)
+
+        tempRelations['Data'] = tempData
+
+        return tempRelations
 
     def craeteEntity(self, data):
         # [Entity]部。いわゆるテーブル情報。
@@ -229,7 +250,7 @@ def generateData(fileName):
     return data
 
 
-def checkDiffLib(l1, l2):
+def diffLib(l1, l2):
     matcher = difflib.SequenceMatcher(None, l1, l2)
 
     tempResults = []
@@ -243,50 +264,108 @@ def checkDiffLib(l1, l2):
 
 def myDiff(d0, d1):
 
-    # https://teratail.com/questions/171217
-
     # Header
-    results = {'header': checkDiffLib(d0.headers, d1.headers)}
-    print(results)
+    results = {'header': diffLib(d0.headers, d1.headers)}
 
     # Manager
-    results['ManagerBase'] = checkDiffLib(d0.manager['BaseInfo'], d1.manager['BaseInfo'])
-    print(results)
+    results['ManagerBase'] = diffLib(d0.manager['BaseInfo'], d1.manager['BaseInfo'])
 
     # Manager2 : タブ情報部分
-    results['ManagerPageInfo'] = checkDiffLib(d0.manager['PageInfo'], d1.manager['PageInfo'])
-    print(results)
+    results['ManagerPageInfo'] = diffLib(d0.manager['PageInfo'], d1.manager['PageInfo'])
 
     # Entity : テーブル情報そもそもの有無を keys を利用して判定
-    results['EntityKeys'] = checkDiffLib(list(d0.entities.keys()), list(d1.entities.keys()))
-    print(results)
+    results['EntityKeys'] = diffLib(list(d0.entities.keys()), list(d1.entities.keys()))
 
     # Entity2 : とりあえず両方に同じテーブル名があるやつを対象に、中身の一致確認
     results['Entities'] = None
-    tempResults = {}
+    tempEntities = {}
     for e in d0.entities:
         if e in d1.entities:
             # ここにきたら両データに存在するってことなので、比較処理まで可能。
-            tempR2 = {'EntityBase': None, 'Fields': None, 'Index': None}
-            tempR2['EntityBase'] = checkDiffLib(d0.entities[e]['BaseInfo'], d1.entities[e]['BaseInfo'])
-            tempR2['Fields'] = checkDiffLib(d0.entities[e]['Field'], d1.entities[e]['Field'])
-            tempR2['Index'] = checkDiffLib(d0.entities[e]['Index'], d1.entities[e]['Index'])
-            # 辞書作っておいてあれですが、上記3項目が全部一致してたら格納しなくていいや
-            if not len(tempR2['EntityBase']) == len(tempR2['Fields']) == len(tempR2['Index']) == 0:
-                tempResults[e] = tempR2
+            tempEnt = {'EntityBase': None, 'Fields': None, 'Index': None}
+            tempEnt['EntityBase'] = diffLib(d0.entities[e]['BaseInfo'], d1.entities[e]['BaseInfo'])
+            tempEnt['Fields'] = diffLib(d0.entities[e]['Field'], d1.entities[e]['Field'])
+            tempEnt['Index'] = diffLib(d0.entities[e]['Index'], d1.entities[e]['Index'])
+            # 上記3項目が全部空っぽ（差分なし）なら格納しなくていいや
+            if not len(tempEnt['EntityBase']) == len(tempEnt['Fields']) == len(tempEnt['Index']) == 0:
+                tempEntities[e] = tempEnt
 
-    results['Entities'] = tempResults
+    results['Entities'] = tempEntities
 
-    # Relation
+    # Relation1 : リレーがそもそも増えたり減ったりを判定。内容の一致確認はその2で。
+    results['RelationKeys'] = diffLib(list(d0.relations.keys()), list(d1.relations.keys()))
 
-    print(results)
+    # Relation2 : キーはあるけど内容が一致してるかどうか
+    tempRelation = {}
+    for e in d0.relations:
+        if e in d1.relations:
+            tempRel = {'Data': diffLib(d0.relations[e]['Data'], d1.relations[e]['Data'])}
+            if not len(tempRel['Data']) == 0:
+                tempRelation[e] = tempRel
+
+    results['Relations'] = tempRelation
+
+    # Comment1 : keys
+    results['CommentKeys'] = diffLib(list(d0.comments.keys()), list(d1.comments.keys()))
+
+    # Comment2 : 内容
+    tempComment = {}
+    for e in d0.comments:
+        if e in d1.comments:
+            tempCom = {'Data': diffLib(d0.comments[e], d1.comments[e])}
+            if not len(tempCom['Data']) == 0:
+                tempComment[e] = tempCom
+
+    results['Comments'] = tempComment
+
+    # Shape1 : keys
+    results['ShapeKeys'] = diffLib(list(d0.shapes.keys()), list(d1.shapes.keys()))
+
+    # Shape2 : 内容
+    tempShape = {}
+    for e in d0.shapes:
+        if e in d1.shapes:
+            tempSh = {'Data': diffLib(d0.shapes[e], d1.shapes[e])}
+            if not len(tempSh['Data']) == 0:
+                tempShape[e] = tempSh
+
+    results['Shapes'] = tempShape
+
+    # Line1 : keys
+    results['LineKeys'] = diffLib(list(d0.lines.keys()), list(d1.lines.keys()))
+
+    # Line2 : 内容
+    tempLine = {}
+    for e in d0.lines:
+        if e in d1.lines:
+            tempLn = {'Data': diffLib(d0.lines[e], d1.lines[e])}
+            if not len(tempLn['Data']) == 0:
+                tempLine[e] = tempLn
+
+    results['Lines'] = tempLine
+
+    # Other1 : keys
+    results['OtherKeys'] = diffLib(list(d0.others.keys()), list(d1.others.keys()))
+
+    # Other2 : 内容
+    tempOther = {}
+    for e in d0.others:
+        if e in d1.others:
+            tempOth = {'Data': diffLib(d0.others[e], d1.others[e])}
+            if not len(tempOth['Data']) == 0:
+                tempOther[e] = tempOth
+
+    results['Others'] = tempOther
+
+    return results
 
 
 def main(files):
     print(files)
     d0 = generateData(files[0])
     d1 = generateData(files[1])
-    myDiff(d0, d1)
+    diffSummary = myDiff(d0, d1)
+    print(diffSummary)
     print('main - end')
 
 

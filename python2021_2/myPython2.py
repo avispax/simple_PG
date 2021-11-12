1a4b5f9f9e6015a30f11c7e990870e471bd807ca
omy <mi_la_n@yahoo.co.jp>
2021-11-12 15:37:10 +0900

    20211112 html分を追加

--- python2021_2/myPython2.py
+++ python2021_2/myPython2.py
@@ -35,20 +35,23 @@ def modify_md_thread(file):
         t1 = '### 入力チェック'
         t2 = '### 業務チェック'
         t3 = '------------------------------------------------------------------------------------------'
+        t4 = '<div class="annotation">*メッセージ表示位置画面 項目項番が「－」ハイフンの場合は共通のメッセージエリアに表示する。</div>'
 
         pos = s.find(t1) + len(t1)
         pos2 = s.find(t2, pos)
         pos3 = s.find('- 定義なし', pos, pos2)
+        pos4 = s.find(t4, pos, pos2)
 
-        if pos3 == -1:
-            s = s[:pos2 - 1] + '<div class="annotation">*メッセージ表示位置画面 項目項番が「－」ハイフンの場合は共通のメッセージエリアに表示する。</div>\n\n' + s[pos2:]
+        if pos3 == -1 and pos4 == -1:
+            s = s[:pos2 - 1] + t4 + '\n\n' + s[pos2:]
 
         pos = s.find(t2) + len(t2)
         pos2 = s.find(t3, pos)
         pos3 = s.find('- 定義なし', pos, pos2)
+        pos4 = s.find(t4, pos, pos2)
 
-        if pos3 == -1:
-            s = s[:pos2 - 1] + '<div class="annotation">*メッセージ表示位置画面 項目項番が「－」ハイフンの場合は共通のメッセージエリアに表示する。</div>\n\n' + s[pos2:]
+        if pos3 == -1 and pos4 == -1:
+            s = s[:pos2 - 1] + t4 + '\n\n' + s[pos2:]
 
     # 3. 最後に<footer>をつける
     if s.find('<footer>以上</footer>') == -1:
@@ -70,55 +73,102 @@ def modify_html_thread(file):
         s = f.read()
 
     # 1. タイトルをIDと名称に更新する。ファイル名をそのまま採用
-    t = '# イトーヨーカドーネットスーパー<br><br>'
-    pos = s.find(t) + len(t)
-    pos2 = s.rfind(' ', pos, s.find('設計書', pos))
+    t = '>イトーヨーカドーネットスーパー<br><br>'
+    pos = s.find(t)
+    pos2 = s.rfind(' ', pos, s.find('設計書</h1>', pos))
     file_name = os.path.basename(file)
-    s = s.replace(s[pos:pos2], file_name[file_name.find('_') + 1: file_name.rfind('.')])
+    s = s.replace(s[pos:pos2], t + file_name[file_name.find('_') + 1: file_name.rfind('.')])
 
     # 2. 画面設計書のチェック項目があれば注釈をつける
     if '画面設計書_' in file:
-        t1 = '### 入力チェック'
-        t2 = '### 業務チェック'
-        t3 = '------------------------------------------------------------------------------------------'
+        t1 = '>入力チェック</h3>'
+        t2 = '>業務チェック</h3>'
+        t3 = '<hr>'
+        t4 = '<div class="annotation">*メッセージ表示位置画面 項目項番が「－」ハイフンの場合は共通のメッセージエリアに表示する。</div>'
 
         pos = s.find(t1) + len(t1)
         pos2 = s.find(t2, pos)
-        pos3 = s.find('- 定義なし', pos, pos2)
+        pos3 = s.find('<li>定義なし</li>', pos, pos2)
+        pos4 = s.find(t4, pos, pos2)
 
-        if pos3 == -1:
-            s = s[:pos2 - 1] + '<div class="annotation">*メッセージ表示位置画面 項目項番が「－」ハイフンの場合は共通のメッセージエリアに表示する。</div>\n\n' + s[pos2:]
+        if pos3 == -1 and pos4 == -1:
+            s = s[:pos2 - 1] + '\n' + t4 + '\n' + s[pos2:]
 
         pos = s.find(t2) + len(t2)
         pos2 = s.find(t3, pos)
-        pos3 = s.find('- 定義なし', pos, pos2)
+        pos3 = s.find('<li>定義なし</li>', pos, pos2)
+        pos4 = s.find(t4, pos, pos2)
 
-        if pos3 == -1:
-            s = s[:pos2 - 1] + '<div class="annotation">*メッセージ表示位置画面 項目項番が「－」ハイフンの場合は共通のメッセージエリアに表示する。</div>\n\n' + s[pos2:]
+        if pos3 == -1 and pos4 == -1:
+            s = s[:pos2 - 1] + '\n' + t4 + '\n' + s[pos2:]
 
     # 3. 最後に<footer>をつける
     if s.find('<footer>以上</footer>') == -1:
-        s = s + '\n<footer>以上</footer>\n'
+
+        pos = s.find('</body>')
+
+        s = s[:pos - 1] + '\n<footer>以上</footer>\n' + s[pos:]
 
     # 現在の「*.html」を退避する。
     new_file = file.replace('.html', '_old.html')
     os.rename(file, new_file)
 
-    # md生成
+    # html生成
     with open(file, mode='w', encoding='utf-8_sig') as f:
         f.write(s)
+
+
+def init():
+
+    # 準備
+    shutil.copytree(SOURCE_DIRECTORY, OUTPUT_DIRECTORY)  # ディレクトリをまるごと別ディレクトリとして同階層にコピー。
+
+    # お掃除 : 「bak」ディレクトリは削除。
+    file_list = glob.glob(OUTPUT_DIRECTORY + '\\**\\bak', recursive=True)
+    for file in file_list:
+        print(file)
+        try:
+            shutil.rmtree(file)
+            continue
+
+        except FileNotFoundError:
+            continue
+
+    # お掃除 : 「作成中」ディレクトリは削除。つーかこういうのもうGitとかで管理して、完成したやつだけコミットしろや。履歴管理は履歴管理サービスに任せぇ！
+    file_list = glob.glob(OUTPUT_DIRECTORY + '\\**\\*作成中', recursive=True)
+    for file in file_list:
+        print(file)
+        try:
+            shutil.rmtree(file)
+            continue
+
+        except FileNotFoundError:
+            continue
+
+    # お掃除 : 「bk」ディレクトリは削除。
+    file_list = glob.glob(OUTPUT_DIRECTORY + '\\**\\bk', recursive=True)
+    for file in file_list:
+        print(file)
+        try:
+            shutil.rmtree(file)
+            continue
+
+        except FileNotFoundError:
+            continue
 
 
 def main():
 
     # 準備
-    shutil.copytree(SOURCE_DIRECTORY, OUTPUT_DIRECTORY)  # ディレクトリをまるごと別ディレクトリとして同階層にコピー。
+    init()
 
     # スタイルシートのコピー
     def copy_style_css():
         print('- ★★ start : スタイルシートのコピー')
         # style.css の一覧を取得
-        ls = glob.glob(OUTPUT_DIRECTORY + '\\**\\style.css', recursive=True)
+        ls = glob.glob(OUTPUT_DIRECTORY + '\\*画面設計書\\**\\style.css', recursive=True)
+        ls = ls + glob.glob(OUTPUT_DIRECTORY + '\\*帳票設計書\\**\\style.css', recursive=True)
+        ls = ls + glob.glob(OUTPUT_DIRECTORY + '\\*メール設計書\\**\\style.css', recursive=True)
 
         # コピー開始
         with ThreadPoolExecutor(max_workers=6) as pool:
@@ -128,7 +178,10 @@ def main():
     def modify_markdown():
         print('- ★★ start : markdown の改造')
         # markdown の一覧を取得
-        ls = glob.glob(OUTPUT_DIRECTORY + '\\**\\*.md', recursive=True)
+        ls = glob.glob(OUTPUT_DIRECTORY + '\\*画面設計書\\**\\*.md', recursive=True)
+        ls = ls + glob.glob(OUTPUT_DIRECTORY + '\\*帳票設計書\\**\\*.md', recursive=True)
+        ls = ls + glob.glob(OUTPUT_DIRECTORY + '\\*メール設計書\\**\\*.md', recursive=True)
+        # ls = glob.glob(OUTPUT_DIRECTORY + '\\**\\*.md', recursive=True)
 
         # 変換開始
         with ThreadPoolExecutor(max_workers=6) as pool:
@@ -138,15 +191,18 @@ def main():
     def modify_html():
         print('- ★★ start : html の改造')
         # markdown の一覧を取得
-        ls = glob.glob(OUTPUT_DIRECTORY + '\\**\\*.html', recursive=True)
+        ls = glob.glob(OUTPUT_DIRECTORY + '\\*画面設計書\\**\\*.html', recursive=True)
+        ls = ls + glob.glob(OUTPUT_DIRECTORY + '\\*帳票設計書\\**\\*.html', recursive=True)
+        ls = ls + glob.glob(OUTPUT_DIRECTORY + '\\*メール設計書\\**\\*.html', recursive=True)
 
         # 変換開始
         with ThreadPoolExecutor(max_workers=6) as pool:
             pool.map(modify_html_thread, ls)
         print('- ★★ end : html の改造')
 
-    # copy_style_css()  # 処理1 : style.css を置き換える
-    # modify_markdown()   # 処理2 : markdown を改造する
+    copy_style_css()  # 処理1 : style.css を置き換える
+    modify_markdown()   # 処理2 : markdown を改造する
+    modify_html()   # 処理3 : html を改造する
 
 
 if __name__ == '__main__':
